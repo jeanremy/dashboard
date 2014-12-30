cc9bf398ed1302ca9b2d053e1bf7499078d1e029
JR <contact@jeanremypraud.com>
2014-12-30 19:40:40 +0100

    ajax add & delete for need in client pages

--- src/Flyd/DashboardBundle/Controller/NeedController.php
+++ src/Flyd/DashboardBundle/Controller/NeedController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Flyd\DashboardBundle\Entity\Need;
 use Flyd\DashboardBundle\Entity\Client;
 use Flyd\DashboardBundle\Form\NeedType;
@@ -13,94 +14,194 @@ use Flyd\DashboardBundle\Form\NeedType;
 class NeedController extends Controller
 {
 	public function addAction(Request $request, $id)
-    {
-        $need = new Need();
-        $em = $this->getDoctrine()->getManager();
-        $client = $em->getRepository('FlydDashboardBundle:Client')->find($id);
+	{
+		$need = new Need();
+		$em = $this->getDoctrine()->getManager();
+		$client = $em->getRepository('FlydDashboardBundle:Client')->find($id);
 
-        $need->setStartDate(new \DateTime() );
-        $need->setDeadline(new \DateTime('+1 month') );
-        $need->setEndDate(new \DateTime('+1 month') );
-        $need->setClient($client);
-
-
-        $form = $this->get('form.factory')->create(new NeedType(), $need);
-
-        if ($form->handleRequest($request)->isValid()) {
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($need);
-          $em->flush();
-
-          $request->getSession()->getFlashBag()->add('notice', 'Besoin bien enregistré.');
-
-          return $this->redirect($this->generateUrl('need_show', array('id' => $need->getId())));
-        }
-
-        return $this->render('FlydDashboardBundle:Need:add.html.twig', array(
-          'form' => $form->createView(),
-          'entity' => $need
-        ));
-    }
-
-    public function showAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('FlydDashboardBundle:Need')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Need entity.');
-        }
-
-        return $this->render('FlydDashboardBundle:Need:show.html.twig', array(
-            'entity' => $entity
-        ));
-    }
-
-     public function editAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $need = $em->getRepository('FlydDashboardBundle:Need')->find($id);
+		$need->setStartDate(new \DateTime() );
+		$need->setDeadline(new \DateTime('+1 month') );
+		$need->setEndDate(new \DateTime('+1 month') );
+		$need->setClient($client);
 
 
-        $form = $this->get('form.factory')->create(new NeedType(), $need);
+		$form = $this->get('form.factory')->create(new NeedType(), $need);
 
-        if ($form->handleRequest($request)->isValid()) {
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($need);
-          $em->flush();
+		if ($form->handleRequest($request)->isValid()) {
+		  $em = $this->getDoctrine()->getManager();
+		  $em->persist($need);
+		  $em->flush();
 
-          $request->getSession()->getFlashBag()->add('notice', 'Besoin bien enregistré.');
+		  $request->getSession()->getFlashBag()->add('notice', 'Besoin bien enregistré.');
 
-          return $this->redirect($this->generateUrl('need_show', array('id' => $need->getId())));
-        }
+		  return $this->redirect($this->generateUrl('need_show', array('id' => $need->getId())));
+		}
 
-        return $this->render('FlydDashboardBundle:Need:add.html.twig', array(
-          'entity' => $need,
-          'form' => $form->createView()
-        ));
-    }
+		return $this->render('FlydDashboardBundle:Need:add.html.twig', array(
+		  'form' => $form->createView(),
+		  'entity' => $need
+		));
+	}
+
+	public function ajaxAddAction()
+	{
+		$request = $this->container->get('request');
+		$params = $this->getRequest()->request->all();
+	    $response = new JsonResponse();
+	    $need = new Need();
+	    $em = $this->getDoctrine()->getManager();
+
+	    if($request->isXmlHttpRequest())
+	    {
+	    	
+        	$form = $this->get('form.factory')->create(new NeedType(), $need);
+
+	    	if ($form->handleRequest($request)->isValid()) {
+	          
+				$em->persist($need);
+				$em->flush();
+
+				// get need show template
+				$needHtml = $this->container->get('templating')->render('FlydDashboardBundle:Need:mini.html.twig', array(
+					'need'=> $need
+				));
+				return $response->setData(array(
+					'code' => 200,
+					'response'=> $needHtml
+				));
+	        }
+	        else {
+        		$errors = $form->getErrors();
+	        	return $response->setData(array(
+					'code' => 500,
+					'response'=> $errors
+				));
+	        }
+		}
+	}
+
+	public function showAction($id)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$entity = $em->getRepository('FlydDashboardBundle:Need')->find($id);
+
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Need entity.');
+		}
+
+		return $this->render('FlydDashboardBundle:Need:show.html.twig', array(
+			'entity' => $entity
+		));
+	}
+
+	 public function editAction(Request $request, $id)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$need = $em->getRepository('FlydDashboardBundle:Need')->find($id);
+
+
+		$form = $this->get('form.factory')->create(new NeedType(), $need);
+
+		if ($form->handleRequest($request)->isValid()) {
+		  $em = $this->getDoctrine()->getManager();
+		  $em->persist($need);
+		  $em->flush();
+
+		  $request->getSession()->getFlashBag()->add('notice', 'Besoin bien enregistré.');
+
+		  return $this->redirect($this->generateUrl('need_show', array('id' => $need->getId())));
+		}
+
+		return $this->render('FlydDashboardBundle:Need:add.html.twig', array(
+		  'entity' => $need,
+		  'form' => $form->createView()
+		));
+	}
 
   
-    public function deleteAction(Request $request, $id)
-    {
-        
-        $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('FlydDashboardBundle:Need')->find($id);
+	public function deleteAction(Request $request, $id)
+	{
+		
+		$em = $this->getDoctrine()->getManager();
+		$entity = $em->getRepository('FlydDashboardBundle:Need')->find($id);
 
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Need entity.');
-        }
-        $client = $entity->getClient();
+		if (!$entity) {
+			throw $this->createNotFoundException('Unable to find Need entity.');
+		}
+		$client = $entity->getClient();
 
-        $em->remove($entity);
-        $em->flush();
-        $request->getSession()->getFlashBag()->add('notice', 'Le besoin a bien été supprimé.');
+		$em->remove($entity);
+		$em->flush();
+		$request->getSession()->getFlashBag()->add('notice', 'Le besoin a bien été supprimé.');
 
 
-        return $this->render('FlydDashboardBundle:Client:show.html.twig', array(
-          'entity' => $client
-        ));
-    }
+		return $this->render('FlydDashboardBundle:Client:show.html.twig', array(
+		  'entity' => $client
+		));
+	}
+
+	public function ajaxDeleteAction()
+	{               
+		$request = $this->container->get('request');
+		$params = $this->getRequest()->request->all();
+	    $response = new JsonResponse();
+
+	    if($request->isXmlHttpRequest())
+	     {
+
+	        $em = $this->container->get('doctrine')->getEntityManager();
+
+	        if($params['element_id'] != '')
+	        {
+	               $qb = $em->createQueryBuilder();
+
+	               $qb->delete('FlydDashboardBundle:Need', 'a')
+	                  ->where("a.id LIKE :element_id")
+	                  ->setParameter('element_id', '%'.$params['element_id'].'%');
+
+	               $query = $qb->getQuery();               
+	               $results = $query->getResult();
+
+			        return $response->setData(array(
+					    'code' => 200,
+					    'response' => 'Need deleted'
+					));
+	        }
+	        else {
+	            return $response->setData(array(
+				    'code' => 500,
+				    'response' => 'Need ID missing'
+				));
+	        }
+
+	    }
+	    else {
+	        return $response->setData(array(
+			    'code' => 500,
+			    'response' => 'Not an ajax request',
+			    'element' => $params['element_id']
+			));
+	    }
+	}
+
+	public function getFormAction($id)
+	{
+        $request = $this->container->get('request');
+	    $response = new JsonResponse();
+	    $need = new Need();
+	    $need->setStartDate(new \DateTime() );
+		$need->setDeadline(new \DateTime('+1 month') );
+		$need->setEndDate(new \DateTime('+1 month') );
+
+	    $em = $this->getDoctrine()->getManager();
+        $client = $em->getRepository('FlydDashboardBundle:Client')->find($id);
+        $need->setClient($client);
+        $form = $this->get('form.factory')->create(new NeedType(), $need);
+
+		return $this->render('FlydDashboardBundle:Need:add.html.twig', array(
+			'form'=> $form->createView()
+		));
+	}	
 }
--- web/bundles/flyddashboard/js/additems.js
+++ web/bundles/flyddashboard/js/additems.js
@@ -7,17 +7,16 @@ $(document).ready(function() {
         e.preventDefault();
         var $this = $(this);
         $this.next().toggle();
-        console.log('e');
     });
 
-    $(document).on('submit', '#flyd_dashboardbundle_address, #flyd_dashboardbundle_contact', function(e){
+    $(document).on('submit', '#flyd_dashboardbundle_address, #flyd_dashboardbundle_contact, #flyd_dashboardbundle_need', function(e){
         e.preventDefault();
         var $this = $(this);
             loadItem($this);
     });
 
     function loadItem($this) {
-        var $parentBloc = $this.parent();
+        var $parentBloc = $this.parent().parent().parent();
 
         $.ajax({
           url:            $this.attr('action'),
@@ -27,21 +26,47 @@ $(document).ready(function() {
             console.log('beforsend');
           },
           success: function(data) {
-            console.log(data);
             if(data['code'] == 200) {
-                $parentBloc.parent().find('.bloc > h2 + .row').append(data['response']);
-                $parentBloc.find('input, select').val('');
+                $parentBloc.find('.bloc > h2 + .row').append(data['response']);
+                $parentBloc.find('.bloc input, .bloc select, .bloc textarea').val('');
             }
             else {
-
-              $('#addresses').prepend('erreur');
+              $parentBloc.parent().find('.bloc > h2 + .row').prepend('erreur');
             }
           },
           error: function(jqXHR, textStatus, errorThrown) {
-            console.log('error');
+            console.log(errorThrown);
           }
         });
 
     }
+
+
+    $(document).on('click', '.remove', function(e){
+      e.preventDefault();
+      var $this = $(this),
+          $parentBloc = $this.parent();
+      $.ajax({
+        url:            $this.attr('href'),
+        type:           "POST",
+        data: {
+          element_id: $this.attr('data-id')
+        },
+        beforeSend: function(data) {
+          console.log('beforsend');
+        },
+        success: function(data) {
+          if(data['code'] == 200) {
+            $parentBloc.fadeOut(400).remove();
+          }
+          else {
+            console.log(data['response']);
+          }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log('error');
+        }
+     });
+    });
 
 });
