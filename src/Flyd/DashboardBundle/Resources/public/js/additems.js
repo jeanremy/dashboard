68534ab8de042616d1e7a391bd1aafaa36be5958
JR <contact@jeanremypraud.com>
2015-02-18 22:48:46 +0100

    c=plugin in the right directory

--- src/Flyd/DashboardBundle/Resources/public/js/additems.js
+++ src/Flyd/DashboardBundle/Resources/public/js/additems.js
@@ -1,83 +1,38 @@
-$(document).ready(function() {
-
-
-
-
-	$(document).on('submit', '#flyd_dashboardbundle_address, #flyd_dashboardbundle_contact, #flyd_dashboardbundle_need', function(e){
-		e.preventDefault();
-		var $this = $(this);
-			loadItem($this, true);
-	});
-
-	$(document).on('submit', '#add_supplier, #add_user', function(e){
-		e.preventDefault();
-		var $this = $(this);
-			loadItem($this, false);
-	});
-
-	/*
-     *
-     * @param: object, bool(true if form has to be empty at the end)
-     */
-	function loadItem($this, empty) {
-		var $parentBloc = $this.parent().parent().parent();
+(function ( $ ) {
+ 
+ 	$.fn.addItems = function( options ) {
+		// Default options
+	    var parentContainer = this.parent().parent().parent();
 
 		$.ajax({
-		  url:            $this.attr('action'),
-		  type:           $this.attr('method'),
-		  data: $this.serialize(),
-		  beforeSend: function(data) {
-		  },
-		  success: function(data) {	
+			url:            this.attr('action'),
+			type:           this.attr('method'),
+			data: this.serialize(),
+			beforeSend: function(data) {
+			},
+			success: function(data) {	
 			if(data.code == 200) {
-				$parentBloc.find('.bloc > h2 + .row').append(data.response);
-				if(empty == true)
-				$parentBloc.find('.bloc input, .bloc select, .bloc textarea').val('');
-				$this.find(':selected').remove();
+				parentContainer.find('.bloc > h2 + .row').append(data.response);
+				if(options.emptyForm === true) {
+					parentContainer.find('.bloc input, .bloc select, .bloc textarea').val('');
+				} else {
+					parentContainer.find(':selected').remove();
+				}
 			}
 			else {
-			  if(empty == false) {
-			  	alert('Erreur');
-			  } else {
-			  	$parentBloc.parent().find('.bloc > h2 + .row').prepend('erreur');
-			  }
+				if(options.emptyForm === false) {
+					alert('Erreur');
+				} else {
+					parentContainer.parent().find('.bloc > h2 + .row').prepend('erreur');
+				}
 			}
-		  },
-		  error: function(jqXHR, textStatus, errorThrown) {
-			console.log(errorThrown);
-		  }
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(errorThrown);
+			}
 		});
 
-	}
-
-
-	$(document).on('click', '.remove', function(e){
-	  e.preventDefault();
-	  var $this = $(this),
-		  $parentBloc = $this.parent();
-	  $.ajax({
-		url:            $this.attr('href'),
-		type:           "POST",
-		data: {
-		  element_id: $this.attr('data-id')
-		},
-		beforeSend: function(data) {
-		  console.log('beforsend');
-		},
-		success: function(data) {
-		console.log(data);
-		  if(data['code'] == 200) {
-			$parentBloc.fadeOut(400).remove();
-		  }
-		  else {
-			console.log(data['response']);
-		  }
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-		  console.log('error');
-		}
-	 });
-	});
-
-
-});
+    	return this;
+    };
+ 
+}( jQuery ));
--- src/Flyd/DashboardBundle/Resources/public/js/main.js
+++ src/Flyd/DashboardBundle/Resources/public/js/main.js
@@ -32,10 +32,51 @@
 			var $this = $(this);
 			$this.next().fadeToggle();
 			$text = $(this).text();
-			console.log($text);
     		$(this).text($text === "+" ? "-":"+");
 		});
 
+		// Add items
+		$(document).on('submit', '#flyd_dashboardbundle_address, #flyd_dashboardbundle_contact, #flyd_dashboardbundle_need', function(e){
+			e.preventDefault();
+			$(this).addItems({
+				emptyForm: true
+			});
+		});
+
+		$(document).on('submit', '#add_supplier, #add_user', function(e){
+			e.preventDefault();
+			$(this).addItems({
+				emptyForm: false
+			});
+		});
+
+		$(document).on('click', '.remove', function(e){
+		  e.preventDefault();
+		  var $this = $(this),
+			  $parentBloc = $this.parent();
+		  $.ajax({
+			url:            $this.attr('href'),
+			type:           "POST",
+			data: {
+			  element_id: $this.attr('data-id')
+			},
+			beforeSend: function(data) {
+			  console.log('beforsend');
+			},
+			success: function(data) {
+			console.log(data);
+			  if(data['code'] == 200) {
+				$parentBloc.fadeOut(400).remove();
+			  }
+			  else {
+				console.log(data['response']);
+			  }
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+			  console.log('error');
+			}
+		 });
+		});
 
 
 
