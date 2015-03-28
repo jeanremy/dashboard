b458a2e7077423b7ab8dde83db66ea6a38ce8dc6
JR <contact@jeanremypraud.com>
2015-03-28 10:56:46 +0100

    moved types in a dedicated folder

---
+++ src/Flyd/DashboardBundle/Form/Type/SupplierType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Flyd\DashboardBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class SupplierType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('job')
+            ->add('entrance', 'date', array(
+                'widget' => 'single_text'
+            ))
+            ->add('origin', 'entity', array(
+                'class' => 'FlydDashboardBundle:Origin',
+                'property' => 'name',
+            ))
+            ->add('save','submit', array(
+                'attr' => array('class' => 'btn--save--reverse'),
+                'label' => 'Enregistrer',
+            ));
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Flyd\DashboardBundle\Entity\Supplier'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'flyd_dashboardbundle_supplier';
+    }
+}
---
+++ src/Flyd/DashboardBundle/Form/Type/TaskType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Flyd\DashboardBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class TaskType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('step', 'choice', array(
+                'choices'   => array(
+                    '0'     => 'Administratif',
+                    '1'     => 'Studio'
+            )));
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Flyd\DashboardBundle\Entity\Task'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'flyd_dashboardbundle_task';
+    }
+}
